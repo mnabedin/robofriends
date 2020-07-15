53342815115dadcdcb3e76fc3bc83248de10c05c
mnabedin <mnabedin@uwaterloo.ca>
2020-07-14 23:00:13 -0400

    final project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList.js';
+import App from './containers/App.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
